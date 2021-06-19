7e75f231c51433cc3cd472042fa84f2974387d31
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-06-19 19:04:37 +0300

    добавлен стейт активной карточки фильма из списка

--- project/src/components/film-list/film-list.jsx
+++ project/src/components/film-list/film-list.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import FilmCard from '../film-card/film-card';
 import PropTypes from 'prop-types';
 import filmProp from '../film/filmProp';
 
 function FilmList(props) {
   const {films} = props;
+  const [activeFilmCard, setActiveFilmCard] = useState(0);
+
   return (
     <div className="catalog__films-list">
-      {films.map((film) => (<FilmCard key={film.id} filmId={film.id} filmName={film.title} previewImage={film.poster}/>))}
+      <p> STATE={activeFilmCard}</p>
+      {films.map((film) => (
+        <FilmCard key = {film.id}
+          filmId = {film.id}
+          filmName = {film.title}
+          previewImage = {film.poster}
+          onMouseEnter = {(target) => {
+            setActiveFilmCard(film.id);}}
+        />
+      ))}
     </div>
   );
 }
