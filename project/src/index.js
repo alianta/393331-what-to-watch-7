c833bb164184eb304e4b506dcb3c525893715b6a
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-06-19 21:09:58 +0300

    добавление моковые данные отзывов

--- project/src/index.js
+++ project/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import films from './mocks/films';
+import reviews from './mocks/reviews';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App films={films} />
+    <App films={films} reviews={reviews}/>
   </React.StrictMode>,
   document.getElementById('root'));
