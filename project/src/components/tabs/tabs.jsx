2c3b78e0c43d29d24d4c9e0d7407493a77986daa
Natalia Khrustaleva <alianta.wd@gmail.com>
2021-06-27 16:21:42 +0300

    добавлены данные на вкладке overview

--- project/src/components/tabs/tabs.jsx
+++ project/src/components/tabs/tabs.jsx
@@ -53,7 +53,33 @@ function FilmInfoData(film, tabName) {
     case TabNames.REVIEWS:
       return 'REVIEWS';
     default:
-      return 'Overview';
+      return (
+        <React.Fragment>
+          <div className="film-rating">
+            <div className="film-rating__score">{film.ratingScore}</div>
+            <p className="film-rating__meta">
+              <span className="film-rating__level">{film.ratingDescription}</span>
+              <span className="film-rating__count">{film.ratingCount} ratings</span>
+            </p>
+          </div>
+
+          <div className="film-card__text">
+            <p>{film.description}</p>
+            <p className="film-card__director"><strong>Director: {film.director}</strong></p>
+            <p className="film-card__starring">
+              <strong>Starring: {film.starring.map((value, id) => {
+                const keyIndex = film.id + id;
+                let coma = '';
+                (id !== film.starring.length-1)? coma=', ':coma='';
+                return (
+                  <React.Fragment key={keyIndex}>{value}{coma}</React.Fragment>
+                );
+              })} and other
+              </strong>
+            </p>
+          </div>
+        </React.Fragment>
+      );
   }
 }
 
