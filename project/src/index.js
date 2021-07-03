4d48bc43b3c7b1c14f8629238eba888ba7a96bb4
Natalia Khrustaleva <alianta.wd@gmail.com>
2021-07-03 11:17:52 +0300

    добавлена поддержка инструмента Redux DevTools

--- project/src/index.js
+++ project/src/index.js
@@ -6,8 +6,12 @@ import reviews from './mocks/reviews';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import {reducer} from './store/reducer';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  composeWithDevTools(),
+);
 
 ReactDOM.render(
   <Provider store={store}>
