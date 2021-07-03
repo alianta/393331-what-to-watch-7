11b256d953bc8b93e3da4eda9850737059d12312
Natalia Khrustaleva <alianta.wd@gmail.com>
2021-07-03 11:15:30 +0300

    инициализация хранилища

--- project/src/index.js
+++ project/src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import films from './mocks/films';
 import reviews from './mocks/reviews';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {reducer} from './store/reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App films={films} reviews={reviews}/>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App films={films} reviews={reviews}/>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root'));
