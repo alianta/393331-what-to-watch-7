570c3335e86be4cbb4a032323968eb0b26e76cbb
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-07-23 23:47:42 +0300

    обновлен код для создания хранилища в связи с переходом на  ReduxToolkit

--- project/src/index.js
+++ project/src/index.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
-import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import rootReducer from './store/root-reducer';
-import {composeWithDevTools} from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
 import {createAPI} from './services/api';
 import {requireAuthorization} from './store/action';
 import {AuthorizationStatus} from './const';
 import {checkAuth, fetchFlms, fetchPromoFilm} from './store/api-actions';
 import {redirect} from './store/middlewares/redirect';
+import {configureStore} from '@reduxjs/toolkit';
 
 const api = createAPI(
   () => store.dispatch(requireAuthorization(AuthorizationStatus.NO_AUTH)),
 );
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(
-    applyMiddleware(thunk.withExtraArgument(api)),
-    applyMiddleware(redirect),
-  ),
-);
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }).concat(redirect),
+});
 
 store.dispatch(checkAuth());
 store.dispatch(fetchFlms());
