38432c4b4639ea158bce8752be060f6dd2a721c8
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-08-01 13:43:28 +0300

    удален лишний Router

--- project/src/components/loading-screen/loading-screen.test.js
+++ project/src/components/loading-screen/loading-screen.test.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import {render} from '@testing-library/react';
-import {Router} from 'react-router-dom';
-import {createMemoryHistory} from 'history';
 import LoadingScreen from './loading-screen';
 
 describe('Component: loading-screen', () => {
   it('should render correctly', () => {
-    const history = createMemoryHistory();
-    const {getByText} = render(
-      <Router history={history}>
-        <LoadingScreen />
-      </Router>,
-    );
-
+    const {getByText} = render(<LoadingScreen />);
     const loadingScreenElement = getByText('Loading ...');
     expect(loadingScreenElement).toBeInTheDocument();
   });
